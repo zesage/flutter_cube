b5ab24f22962dfcf16cabd1c95604d2589648db5
Mark Hu <zesage@gmail.com>
2019-12-19 14:10:38 +0800

    add cube widget

--- lib/src/widget.dart
+++ lib/src/widget.dart
@@ -1,13 +1,84 @@
-import 'package:flutter/widgets.dart';
+import 'dart:ui';
+import 'package:flutter/widgets.dart' hide Image;
+import 'package:vector_math/vector_math_64.dart';
+import 'scene.dart';
+
+typedef void SceneCreatedCallback(Scene scene);
 
 class Cube extends StatefulWidget {
+  Cube({
+    Key key,
+    this.onSceneCreated,
+    this.onObjectCreated,
+  }) : super(key: key);
+
+  final SceneCreatedCallback onSceneCreated;
+  final ObjectCreatedCallback onObjectCreated;
+
   @override
   _CubeState createState() => _CubeState();
 }
 
 class _CubeState extends State<Cube> {
+  Scene scene;
+  Offset _lastFocalPoint;
+
+  void _handleScaleStart(ScaleStartDetails details) {
+    _lastFocalPoint = details.localFocalPoint;
+  }
+
+  void _handleScaleUpdate(ScaleUpdateDetails details) {
+    scene.camera.trackBall(toVector2(_lastFocalPoint), toVector2(details.localFocalPoint), 1.5);
+    _lastFocalPoint = details.localFocalPoint;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    scene = Scene(
+      onUpdate: () => setState(() {}),
+      onObjectCreated: widget.onObjectCreated,
+    );
+    if (widget.onSceneCreated != null) widget.onSceneCreated(scene);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        scene.camera.viewportWidth = constraints.maxWidth;
+        scene.camera.viewportHeight = constraints.maxHeight;
+        return GestureDetector(
+          onScaleStart: _handleScaleStart,
+          onScaleUpdate: _handleScaleUpdate,
+          child: CustomPaint(
+            painter: _CubePainter(scene),
+            size: Size(constraints.maxWidth, constraints.maxHeight),
+          ),
+        );
+      },
+    );
   }
+}
+
+class _CubePainter extends CustomPainter {
+  final Scene _scene;
+  const _CubePainter(this._scene);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    _scene.render(canvas, size);
+  }
+
+  // We should repaint whenever the board changes, such as board.selected.
+  @override
+  bool shouldRepaint(_CubePainter oldDelegate) {
+    return true;
+  }
+}
+
+/// Convert Offset to Vector2
+Vector2 toVector2(Offset value) {
+  return Vector2(value.dx, value.dy);
 }
