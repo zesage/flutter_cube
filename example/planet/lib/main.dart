c07115feb3a7e427300f7682ada19b9b3f36c68c
Mark Hu <zesage@gmail.com>
2020-03-10 00:55:32 +0800

    add planet example

---
+++ example/planet/lib/main.dart
@@ -0,0 +1,120 @@
+import 'dart:ui' as ui;
+import 'dart:math' as math;
+import 'package:flutter/material.dart';
+import 'package:flutter_cube/flutter_cube.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Planet',
+      theme: ThemeData.dark(),
+      home: MyHomePage(title: 'Planet'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+  Scene _scene;
+  Object _earth;
+  Object _stars;
+  AnimationController _controller;
+
+  void generateSphereObject(Object parent, String name, double radius, bool backfaceCulling, String texturePath) async {
+    final Mesh mesh = await generateSphereMesh(radius: radius, texturePath: texturePath);
+    parent.add(Object(name: name, mesh: mesh, backfaceCulling: backfaceCulling));
+    _scene.updateTexture();
+  }
+
+  void _onSceneCreated(Scene scene) {
+    _scene = scene;
+    _scene.camera.position.z = 16;
+
+    // model from https://free3d.com/3d-model/planet-earth-99065.html
+    // _earth = Object(name: 'earth', scale: Vector3(10.0, 10.0, 10.0), backfaceCulling: true, fileName: 'assets/earth/earth.obj');
+
+    // create by code
+    _earth = Object(name: 'earth', scale: Vector3(10.0, 10.0, 10.0));
+    generateSphereObject(_earth, 'surface', 0.485, true, 'assets/earth/4096_earth.jpg');
+    generateSphereObject(_earth, 'clouds', 0.5, true, 'assets/earth/4096_clouds.png');
+    _scene.world.add(_earth);
+
+    // texture from https://www.solarsystemscope.com/textures/
+    _stars = Object(name: 'stars', scale: Vector3(2000.0, 2000.0, 2000.0));
+    generateSphereObject(_stars, 'surface', 0.5, false, 'assets/stars/2k_stars.jpg');
+    _scene.world.add(_stars);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(duration: Duration(milliseconds: 30000), vsync: this)
+      ..addListener(() {
+        if (_earth != null) {
+          _earth.rotation.y = _controller.value * 360;
+          _earth.updateTransform();
+          _scene.update();
+        }
+      })
+      ..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Cube(onSceneCreated: _onSceneCreated),
+    );
+  }
+}
+
+Future<Mesh> generateSphereMesh({num radius = 0.5, int latSegments = 32, int lonSegments = 64, String texturePath}) async {
+  int count = (latSegments + 1) * (lonSegments + 1);
+  List<Vector3> vertices = List<Vector3>(count);
+  List<Offset> texcoords = List<Offset>(count);
+  List<Polygon> indices = List<Polygon>(latSegments * lonSegments * 2);
+
+  int i = 0;
+  for (int y = 0; y <= latSegments; ++y) {
+    final double v = y / latSegments;
+    final double sv = math.sin(v * math.pi);
+    final double cv = math.cos(v * math.pi);
+    for (int x = 0; x <= lonSegments; ++x) {
+      final double u = x / lonSegments;
+      vertices[i] = Vector3(radius * math.cos(u * math.pi * 2.0) * sv, radius * cv, radius * math.sin(u * math.pi * 2.0) * sv);
+      texcoords[i] = Offset(1.0 - u, 1.0 - v);
+      i++;
+    }
+  }
+
+  i = 0;
+  for (int y = 0; y < latSegments; ++y) {
+    final int base1 = (lonSegments + 1) * y;
+    final int base2 = (lonSegments + 1) * (y + 1);
+    for (int x = 0; x < lonSegments; ++x) {
+      indices[i++] = Polygon(base1 + x, base1 + x + 1, base2 + x);
+      indices[i++] = Polygon(base1 + x + 1, base2 + x + 1, base2 + x);
+    }
+  }
+
+  ui.Image texture = await loadImageFromAsset(texturePath);
+  final Mesh mesh = Mesh(vertices: vertices, texcoords: texcoords, indices: indices, texture: texture, texturePath: texturePath);
+  return mesh;
+}
